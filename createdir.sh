9697bb6dd19a83554c4c83f2f050e0929775d049
Ibraimov Alexey <ibraimov@techart.ru>
2024-05-31 14:11:25 +0300

    fix

--- createdir.sh
+++ createdir.sh
@@ -3,12 +3,14 @@
 cd /pdf2html
 for file in *.pdf; do
 		if [[ ${file%.pdf} != "*" ]]; then
-				/pdf2htmlEX/pdf2htmlEX --zoom 1.3 --last-page 5 "$file" "result/_${file%.pdf}.html"
-				sed -E 's/<span[^>]*_ _[0-9]+[^>]*>([^<]*)<\/span>([^<0-9]{1})/\1\2/g' "result/_${file%.pdf}.html" > "result/full_${file%.pdf}.html"
+#				/pdf2htmlEX/pdf2htmlEX --zoom 1.3 --last-page 5 "$file" "result/_${file%.pdf}.html"
+				/pdf2htmlEX/pdf2htmlEX --zoom 1 --last-page 100 --embed-css 1 --embed-font 1 --embed-javascript 0 "$file" "result/_${file%.pdf}.html"
+
+#				sed -E 's/<span[^>]*_ _[0-9]+[^>]*>([^<]*)<\/span>([^<0-9]{1})/\1\2/g' "result/_${file%.pdf}.html" > "result/full_${file%.pdf}.html"
 #				sed -E 's/<span[^>]*_ _[0-9]+[^>]*>([ -]*)<\/span>/\1/g' "result/full_${file%.pdf}.html" > "result/_${file%.pdf}.html"
-				sed -E 's/<span[^>]*[^>]*>([ -.]+)<\/span>/\1/g' "result/full_${file%.pdf}.html" > "result/_${file%.pdf}.html"
+#				sed -E 's/<span[^>]*[^>]*>([ -.]+)<\/span>/\1/g' "result/full_${file%.pdf}.html" > "result/_${file%.pdf}.html"
 				sed -E 's/([. ]+)(h[0-9]+)/\1doc-\2/g' "result/_${file%.pdf}.html" > "result/full_${file%.pdf}.html"
-				rm "result/_${file%.pdf}.html"
+#				rm "result/_${file%.pdf}.html"
 				sed -i 's/[\x00-\x09\x0B-\x1F\x7F]//g' "result/full_${file%.pdf}.html"
 #				sed -i 's/[^a-zA-Z0-9<>\/=".,#\[\]&@\*\? ]//g' "result/full_${file%.pdf}.html"
 				sed -i 's// /g' "result/full_${file%.pdf}.html"
@@ -25,11 +27,13 @@ for file in *.pdf; do
 				sed -E 's/([^<]{1})(html|body)[_]+/\1pdf2html__\2/g' "result/${file%.pdf}.html" > "result/_${file%.pdf}.html"
 				sed -E 's/<body>/<div class="b-document-pdf2html__body">/g' "result/_${file%.pdf}.html" > "result/${file%.pdf}.html"
 				sed -i 's/<\/body>/<\/div">/g' "result/_${file%.pdf}.html"
+#				sed -i 's/<\/*meta[^>]*>//g' "result/${file%.pdf}.html"
 				#sed -E 's/#(.*)\*\///g' "result/_${file%.pdf}.html" > "result/md_${file%.pdf}.html"
 				sed -i 's/<[!]DOCTYPE.*>//g' "result/${file%.pdf}.html"
 				sed -i 's/<\/*head>//g' "result/${file%.pdf}.html"
 
 				rm "result/_${file%.pdf}.html"
+				rm "result/full_${file%.pdf}.html"
 
 				rm "$file"
 
